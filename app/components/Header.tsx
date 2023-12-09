8cc03e074cb20b1553faa5615ea45fb3711d54fa
Darhniel <nnadidarhniel20@gmail.com>
2023-12-09 02:47:25 +0100

    adjusted logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link"
 import { useState } from 'react'
 import Image from "next/image";
-import logo from '../../public/movie-buddy.png'
+import logo from '@/app/movie-buddy.png'
 
 export default function Header() {
     const [mobile, setMobile] = useState(false);
